23fdf76a69c98a118a496e22f566a5fe9d7522d9
Barry Hanna <barryahanna@gmail.com>
2023-07-02 19:28:35 +0100

    Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	const [showFullDetails, setShowFullDetails] = React.useState(false);
+
+	return (
+		<div style={blogStyle}>
+			{/* <pre>{JSON.stringify(blog, null, 2)}</pre> */}
+			{blog.title} {blog.author}{' '}
+			<button onClick={() => setShowFullDetails(!showFullDetails)}>
+				{showFullDetails ? 'hide' : 'view'}
+			</button>
+			{showFullDetails && (
+				<>
+					<p>{blog.url}</p>
+					<p>
+						{blog.likes} <button>like</button>
+					</p>
+					<p>{blog?.user?.name}</p>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Blog;
