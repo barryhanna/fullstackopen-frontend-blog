9c2eb259e0e953a407ea205655d7f10c8d8bfd6e
Barry Hanna <barryahanna@gmail.com>
2023-07-06 11:20:51 +0100

    Exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,17 +1,41 @@
 describe('Blog app', () => {
 	beforeEach(function () {
 		cy.request('POST', 'http://localhost:3003/api/testing/reset');
-		// const user = {
-		// 	name: 'DaveDave',
-		// 	username: 'dave',
-		// 	password: 'davedave',
-		// };
-		// cy.request('POST', 'http://localhost:3003/api/users/', user);
+		const user = {
+			name: 'DaveDave',
+			username: 'dave',
+			password: 'davedave',
+		};
+		cy.request('POST', 'http://localhost:3003/api/users/', user);
 		cy.visit('http://localhost:3000');
 	});
 
 	it('front page can be opened', function () {
 		cy.visit('http://localhost:3000');
 		cy.contains('Login');
+	});
+
+	describe('Login', function () {
+		it('succeeds with correct credentials', function () {
+			cy.visit('http://localhost:3000');
+			cy.contains('Login').click();
+			cy.get('#username').type('dave');
+			cy.get('#password').type('davedave');
+			cy.get('#loginBtn').click();
+			cy.contains('dave is logged in');
+		});
+
+		it('fails with wrong credentials', function () {
+			cy.visit('http://localhost:3000');
+			cy.contains('Login').click();
+			cy.get('#username').type('dave');
+			cy.get('#password').type('wrongdave');
+			cy.get('#loginBtn').click();
+			cy.contains('Wrong username or password').and(
+				'have.css',
+				'color',
+				'rgb(206, 70, 70)'
+			);
+		});
 	});
 });
--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,37 +1,41 @@
-import React, { useImperativeHandle } from 'react'
-import PropTypes from 'prop-types'
+import React, { useImperativeHandle } from 'react';
+import PropTypes from 'prop-types';
 
 const Togglable = (props, refs) => {
-  const { buttonText, children } = props
-  const [visible, setVisible] = React.useState(false)
+	const { buttonText, children } = props;
+	const [visible, setVisible] = React.useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+	const hideWhenVisible = { display: visible ? 'none' : '' };
+	const showWhenVisible = { display: visible ? '' : 'none' };
 
-  const toggleVisibility = () => setVisible(!visible)
+	const toggleVisibility = () => setVisible(!visible);
 
-  useImperativeHandle(refs, () => {
-    return {
-      toggleVisibility,
-    }
-  })
+	useImperativeHandle(refs, () => {
+		return {
+			toggleVisibility,
+		};
+	});
 
-  return (
-    <div>
-      <div style={hideWhenVisible}>
-        <button onClick={() => setVisible(true)}>{buttonText}</button>
-      </div>
-      <div style={showWhenVisible}>
-        {children}
-        <button onClick={() => setVisible(false)}>Cancel</button>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div>
+			<div style={hideWhenVisible}>
+				<button id={buttonText} onClick={() => setVisible(true)}>
+					{buttonText}
+				</button>
+			</div>
+			<div style={showWhenVisible}>
+				{children}
+				<button id="cancel" onClick={() => setVisible(false)}>
+					Cancel
+				</button>
+			</div>
+		</div>
+	);
+};
 
 Togglable.propTypes = {
-  props: PropTypes.object.isRequired,
-  refs: PropTypes.object.isRequired,
-}
+	props: PropTypes.object.isRequired,
+	refs: PropTypes.object.isRequired,
+};
 
-export default React.forwardRef(Togglable)
+export default React.forwardRef(Togglable);
