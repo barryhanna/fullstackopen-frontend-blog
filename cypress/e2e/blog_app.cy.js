1f0dc6ceead30f8203ad665ab3be737ca0261f6e
Barry Hanna <barryahanna@gmail.com>
2023-07-06 12:34:59 +0100

    Exercise 5.19

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -38,4 +38,26 @@ describe('Blog app', () => {
 			);
 		});
 	});
+
+	describe('When logged in', function () {
+		beforeEach(function () {
+			cy.login({ username: 'dave', password: 'davedave' });
+		});
+
+		it('A blog can be created', function () {
+			// cy.request('POST', 'http://localhost:3003/api/blogs', {
+			// 	title: 'Test Blog',
+			// 	author: 'Test Blog Author',
+			// 	url: 'http://localhost',
+			// });
+			// cy.contains('Test Blog');
+			cy.visit('http://localhost:3000');
+			cy.contains('Add New Blog').click();
+			cy.get('#title').type('Test Blog');
+			cy.get('#author').type('Test Blog Author');
+			cy.get('#url').type('http://localhost');
+			cy.contains('Create').click();
+			cy.contains('Test Blog');
+		});
+	});
 });
